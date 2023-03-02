8aa114868664a6827f29cdefacce64fb569f6e0c
Rod <rodtbowen@gmail.com>
2023-03-02 09:17:16 -0700

    3. Testing Contracts

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -12,7 +12,7 @@ contract Token {
 	constructor(string memory _name, string memory _symbol, uint256 _totalSupply) {
 		name = _name;
 		symbol = _symbol;
-		totalSupply = _totalSupply * (10**decimals); // 10,000,000 x 10^18
+		totalSupply = _totalSupply * (10**decimals);
 
 	}
 }
