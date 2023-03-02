8253c2f396eee9d88b38bf84a912f03e192c2bfa
Rod <rodtbowen@gmail.com>
2023-03-02 12:36:18 -0700

    5. Token Transfers

--- test/Token.js
+++ test/Token.js
@@ -5,11 +5,11 @@ const tokens = (n)=> {
   return ethers.utils.parseEther(n.toString(), 'ether')
 }
 
-
 describe('Token', () => {
   let token, 
       accounts,
-      deployer
+      deployer,
+      receiver
 
   beforeEach( async () => {
     const Token = await ethers.getContractFactory('Token')
@@ -17,6 +17,7 @@ describe('Token', () => {
 
     accounts = await ethers.getSigners()
     deployer = accounts[0]
+    receiver = accounts[1]
   })
 
   describe('Deployment', () => {
@@ -24,8 +25,6 @@ describe('Token', () => {
     const symbol = 'RODD'
     const decimals = '18'
     const totalSupply = tokens('10000000')
-   
-
 
     it('has correct name', async () => {
       expect(await token.name()).to.equal(name)
@@ -45,6 +44,51 @@ describe('Token', () => {
 
     it('assigns total supply to deployer', async () => {
       expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+    })
+  })
+
+  describe('Sending Tokens', () => {
+    let amount,
+        transaction,
+        result
+
+    describe('Success', () => {
+
+      beforeEach(async() => {
+        amount = tokens(100)
+        transaction = await token.connect(deployer).transfer(receiver.address, amount)
+        result = await transaction.wait()
+      })
+
+      it('transfers token balances', async () => {
+        expect(await token.balanceOf(deployer.address)).to.equal(tokens(9999900))
+        expect(await token.balanceOf(receiver.address)).to.equal(amount)
+      })
+
+      it('emits a transfer event', async () => {
+        const event = result.events[0]
+        expect(event.event).to.equal('Transfer')
+
+        const args = event.args
+        expect(args.from).to.equal(deployer.address)
+        expect(args.to).to.equal(receiver.address)
+        expect(args.value).to.equal(amount)
+      })
+    
+
+    })
+
+    describe('Failure', () => {
+      it('rejects insufficient balances', async () => {
+        const invalidAmount = tokens(100000000)
+        await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+      })
+
+      it('rejects invalid recipents', async () => {
+        const amount = tokens(100)
+        await expect(token.connect(deployer).transfer('0x0000000000000000000000000000000000000000', amount)).to.be.reverted
+      })
+
     })
 
   })
